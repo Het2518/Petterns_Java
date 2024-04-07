bdd7e76181ab18564064c1b447a4dacffd1a1699
Het Monpara <131437054+Het2518@users.noreply.github.com>
2024-04-07 23:55:07 +0530

    pettern added

---
+++ Patterns.java
@@ -0,0 +1,140 @@
+public class Patterns {
+
+    public static void main(String[] args) {
+        pattern1(5);
+        System.out.println();
+        pattern2(5);
+        System.out.println();
+        pattern3(5);
+        System.out.println();
+        pattern4(5);
+        System.out.println();
+        pattern5(5);
+        System.out.println();
+        pattern6(5);
+        System.out.println();
+        pattern7(5);
+        System.out.println();
+        pattern8(5);
+        System.out.println();
+        pattern9(5);
+        System.out.println();
+        pattern10(5);
+    }
+
+    static void pattern1(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern2(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern3(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = n; col >= n - row + 1; col--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern4(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern5(int n) {
+        for (int row = 1; row <= (2 * n) - 1; row++) {
+            int totalColsInRow = row > n ? 2 * n - row : row;
+            for (int col = 1; col <= totalColsInRow; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern6(int n) {
+        for (int row = 0; row < 2 * n; row++) {
+            int totalColsInRow = row > n ? 2 * n - row : row;
+
+            int noOfSpaces = n - totalColsInRow;
+            for (int space = 0; space < noOfSpaces; space++) {
+                System.out.print(" ");
+            }
+            for (int col = 0; col < totalColsInRow; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern7(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int space = 0; space < n - row; space++) {
+                System.out.print("  ");
+            }
+
+            for (int col = row; col >= 1; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern8(int n) {
+        for (int row = 1; row <= (2 * n); row++) {
+            int c = row > n ? 2 * n - row : row;
+            for (int space = 0; space < n - c; space++) {
+                System.out.print("  ");
+            }
+            for (int col = c; col >= 1; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= c; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    static void pattern9(int n) {
+        for (int row = 1; row <= 2 * n; row++) {
+            for (int col = 1; col <= 2 * n; col++) {
+                int atEveryIndex = Math.min(Math.min(row, col), Math.min(2 * n - row + 1, 2 * n - col + 1));
+                System.out.print(atEveryIndex + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern10(int n) {
+        int original_N = n;
+        n = 2 * n;
+        for (int row = 1; row <= n - 1; row++) {
+            for (int col = 1; col <= n - 1; col++) {
+                int atEveryIndex = Math.max(Math.max(row, col), Math.max(n - row, n - col)) - original_N;
+                System.out.print(atEveryIndex + " ");
+            }
+            System.out.println();
+        }
+    }
+}
